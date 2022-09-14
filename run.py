0ed5382103981e31631cba05a3ec63a723e4407b
R Moruju <morujurazvan@gmail.com>
2022-09-14 18:36:57 +0000

    Retrieve the latest stock  numbers from spreadsheet

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -53,7 +54,7 @@ def validate_data(values):
     return True
 
 
-def update_sles_worksheet(data):
+def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided.
     """
@@ -63,6 +64,29 @@ def update_sles_worksheet(data):
     print("Sales worsheet update successfully. \n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sles_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock ad calculate the surplus for each item type.
+    
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("welcome to Love Sandwiches Data Automation")
+main()
